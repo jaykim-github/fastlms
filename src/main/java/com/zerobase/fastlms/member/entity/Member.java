08272069e47cbc2c1e0fa66f08f401c49d6442d5
jaykim-github <jaykeem713@gmail.com>
2022-11-05 04:06:23 +0900

    2022.11.05 : 회원가입 및 인증메일 구현 수정...

--- src/main/java/com/zerobase/fastlms/member/entity/Member.java
+++ src/main/java/com/zerobase/fastlms/member/entity/Member.java
@@ -1,11 +1,17 @@
 package com.zerobase.fastlms.member.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Data
 @Entity
 public class Member {
@@ -17,4 +23,9 @@ public class Member {
     private String phone;
     private String password;
     private LocalDateTime regDt;
+
+    private boolean emailAuthYn;
+    private LocalDateTime emailAuthDt;
+    private String emailAuthKey;
+
 }
