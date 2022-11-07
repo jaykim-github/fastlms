a4796e79ec5642962e37274a884350d734fb68a1
jaykim-github <jaykeem713@gmail.com>
2022-11-08 01:38:56 +0900

    2022.11.08 : 로그인,아웃 구현

--- src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
+++ src/main/java/com/zerobase/fastlms/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +20,11 @@ import java.time.LocalDateTime;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @RequestMapping("/member/login")
+    public String login() {
+        return "member/login";
+    }
 
     @GetMapping(value = "/member/register")
     public String register() {
@@ -46,5 +52,10 @@ public class MemberController {
         model.addAttribute("result", result);
 
         return "member/email_auth";
+    }
+
+    @GetMapping("/member/info")
+    public String memberInfo() {
+        return "member/info";
     }
 }
--- src/main/java/com/zerobase/fastlms/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/zerobase/fastlms/member/service/impl/MemberServiceImpl.java
@@ -2,14 +2,23 @@ package com.zerobase.fastlms.member.service.impl;
 
 import com.zerobase.fastlms.components.MailComponents;
 import com.zerobase.fastlms.member.entity.Member;
+import com.zerobase.fastlms.member.exception.MemberNotEmailAuthException;
 import com.zerobase.fastlms.member.model.MemberInput;
 import com.zerobase.fastlms.member.repository.MemberRepository;
 import com.zerobase.fastlms.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import javax.swing.plaf.OptionPaneUI;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,6 +35,8 @@ public class MemberServiceImpl implements MemberService {
         if (optionalMember.isPresent()) {
             return false;
         }
+
+        String encPassword = BCrypt.hashpw(parameter.getPassword(), BCrypt.gensalt());
         String uuid = UUID.randomUUID().toString();
 
         Member member = Member.builder()
@@ -33,7 +44,7 @@ public class MemberServiceImpl implements MemberService {
                 .userName(parameter.getUserName())
                 .phone(parameter.getPhone())
                 .regDt(LocalDateTime.now())
-                .password(parameter.getPassword())
+                .password(encPassword)
                 .emailAuthYn(false)
                 .emailAuthKey(uuid)
                 .build();
@@ -65,6 +76,25 @@ public class MemberServiceImpl implements MemberService {
         memberRepository.save(member);
 
         return true;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> optionalMember = memberRepository.findById(username);
+        if(!optionalMember.isPresent()){
+            throw new UsernameNotFoundException("회원 정보가 존재하지 않습니다.");
+        }
+
+        Member member = optionalMember.get();
+        if(!member.isEmailAuthYn()){
+            throw new MemberNotEmailAuthException("이메일 활성화 이후에 로그인 해주세요.");
+        }
+
+        List<GrantedAuthority> grandtedAuthorities = new ArrayList<>();
+
+        grandtedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new User(member.getUserId(), member.getPassword(), grandtedAuthorities);
     }
 }
 
